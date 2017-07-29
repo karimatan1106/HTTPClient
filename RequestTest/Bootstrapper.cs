41dbd02530d986bda4ef69deb68de3d2f0ca9017
karimata1106 <karimata1106@gmail.com>
2017-07-29 23:11:03 +0900

    commit

---
+++ RequestTest/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using RequestTest.Views;
+using System.Windows;
+
+namespace RequestTest
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ RequestTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using AngleSharp.Parser.Html;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Net.Http;
+
+namespace RequestTest.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        #region コマンド
+        public DelegateCommand HttpCommand { get; set; }
+        #endregion
+
+        #region フィールド
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly HtmlParser _htmlParser = new HtmlParser();
+        #endregion
+
+        #region コンストラクタ
+        public MainWindowViewModel()
+        {
+            HttpCommand = new DelegateCommand(Fuga);
+        }
+        #endregion
+
+        #region メソッド
+        private async void Fuga()
+        {
+            var htmlString = await _httpClient.GetStringAsync("https://www.dailyfx.com/forex-rates");
+            // AngleSharp.Parser.Html.HtmlParserオブジェクトにHTMLをパースさせる
+            var parse = await _htmlParser.ParseAsync(htmlString);
+
+            //#EURUSDは見つかる
+            var eurUsd = parse.QuerySelector("#EURUSD")?.InnerHtml;
+
+            //#eurusd-priceAskが見つからない
+            var priceAsk = parse.QuerySelector("#eurusd-priceAsk")?.InnerHtml;
+        }
+        #endregion
+    }
+}
